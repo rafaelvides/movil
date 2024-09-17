fa162853dd19b8c61ff5462c0b9fc1a1bf02fbd1
Henry09Ramos <henryjeremiasramos09@gmail.com>
2024-09-17 08:50:25 -0600

    Add a file named of_normal_details.tsx
    In the file I show the details of the different types of clients

---
+++ offline/components/customer_details/of_normal_customer.tsx
@@ -0,0 +1,212 @@
+import stylesGlobals from "@/components/Global/styles/StylesAppComponents";
+import { useClientOfflineStore } from "@/offline/store/customer_offline.store";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {  useEffect, useMemo } from "react";
+import { ScrollView, Text, View } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+const Of_normal_customer = ({
+  id,
+  closeModal,
+}: {
+  id: number;
+  closeModal: () => void;
+}) => {
+  const { clientList, OnGetClientsList } = useClientOfflineStore();
+
+
+  useEffect(() => {
+    OnGetClientsList();
+  }, []);
+  const customer_list = useMemo(() => {
+    return clientList.find((item) => item.id === id);
+  }, [clientList, id]);
+
+  console.log("dataaaaaaaaaaaaaaaaaaaaaa", customer_list, id);
+  return (
+    <>
+      <StatusBar style="light" backgroundColor={"#AFB0B1"} />
+      <View
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#AFB0B1",
+        }}
+      >
+        <MaterialCommunityIcons
+          name="close"
+          color={"white"}
+          onPress={() => closeModal()}
+          size={26}
+          style={stylesGlobals.materialIconsStyle}
+        />
+
+        <View
+          style={{
+            flexDirection: "row",
+          }}
+        >
+          <Text style={{ ...stylesGlobals.titleDetailsCustomer }}>
+            {`Detalles cliente ${
+              customer_list?.esContribuyente ? "contribuyente" : "normal"
+            }`}
+          </Text>
+        </View>
+        <View style={stylesGlobals.viewContent}>
+          <ScrollView
+            style={{
+              marginBottom: 50,
+            }}
+          >
+            
+            <View style={stylesGlobals.viewInMaterial}>
+              <MaterialCommunityIcons
+                name="account"
+                color={"#AFB0B1"}
+                size={30}
+                style={stylesGlobals.iconsStyles}
+              />
+              <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.nombre}
+              </Text>
+            </View>
+            {customer_list?.esContribuyente&&(
+                <View style={stylesGlobals.viewInMaterial}>
+                <MaterialCommunityIcons
+                  name="card-account-details"
+                  color={"#AFB0B1"}
+                  size={30}
+                  style={stylesGlobals.iconsStyles}
+                />
+                <Text style={stylesGlobals.textIconStyle}>
+                  {customer_list?.nombreComercial}
+                </Text>
+              </View>
+            )}
+
+            <View style={stylesGlobals.viewInMaterial}>
+              <MaterialCommunityIcons
+                name="email-outline"
+                color={"#AFB0B1"}
+                size={30}
+                style={stylesGlobals.iconsStyles}
+              />
+              <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.correo}
+              </Text>
+            </View>
+
+            <View style={stylesGlobals.viewInMaterial}>
+              <MaterialCommunityIcons
+                name="phone"
+                color={"#AFB0B1"}
+                size={30}
+                style={stylesGlobals.iconsStyles}
+              />
+              <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.telefono}
+              </Text>
+            </View>
+            <View style={stylesGlobals.viewInMaterial}>
+              <MaterialCommunityIcons
+                name="card-text"
+                color={"#AFB0B1"}
+                size={30}
+                style={stylesGlobals.iconsStyles}
+              />
+              <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.tipoDocumento}
+              </Text>
+            </View>
+            <View style={stylesGlobals.viewInMaterial}>
+              <MaterialCommunityIcons
+                name="checkbook"
+                color={"#AFB0B1"}
+                size={30}
+                style={stylesGlobals.iconsStyles}
+              />
+              <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.numDocumento}
+              </Text>
+            </View>
+            {customer_list?.esContribuyente&&(
+                <View style={stylesGlobals.viewInMaterial}>
+                <MaterialCommunityIcons
+                  name="card-bulleted"
+                  color={"#AFB0B1"}
+                  size={30}
+                  style={stylesGlobals.iconsStyles}
+                />
+                <Text style={stylesGlobals.textIconStyle}>
+                {`NRC: ${customer_list.nrc}`}
+                </Text>
+              </View>
+            )}
+              {customer_list?.esContribuyente&&(
+                <View style={stylesGlobals.viewInMaterial}>
+                <MaterialCommunityIcons
+                  name="book-open"
+                  color={"#AFB0B1"}
+                  size={30}
+                  style={stylesGlobals.iconsStyles}
+                />
+                <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.descActividad}
+                </Text>
+              </View>
+            )}
+            {customer_list?.esContribuyente&&(
+                <View style={stylesGlobals.viewInMaterial}>
+                <MaterialCommunityIcons
+                  name="checkbox-marked"
+                  color={"#AFB0B1"}
+                  size={30}
+                  style={stylesGlobals.iconsStyles}
+                />
+                <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.tipoContribuyente}
+                </Text>
+              </View>
+            )}
+
+            <View style={stylesGlobals.viewInMaterial}>
+              <MaterialCommunityIcons
+                name="card-bulleted-outline"
+                color={"#AFB0B1"}
+                size={30}
+                style={stylesGlobals.iconsStyles}
+              />
+              <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.nombreDepartamento}
+              </Text>
+            </View>
+            <View style={stylesGlobals.viewInMaterial}>
+              <MaterialCommunityIcons
+                name="bank"
+                color={"#AFB0B1"}
+                size={30}
+                style={stylesGlobals.iconsStyles}
+              />
+              <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.nombreMunicipio}
+              </Text>
+            </View>
+            <View style={{ ...stylesGlobals.viewInMaterial, marginBottom: 30 }}>
+              <MaterialCommunityIcons
+                name="clipboard-outline"
+                color={"#AFB0B1"}
+                size={30}
+                style={stylesGlobals.iconsStyles}
+              />
+              <Text style={stylesGlobals.textIconStyle}>
+                {customer_list?.complemento}
+              </Text>
+            </View>
+          </ScrollView>
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default Of_normal_customer;
